5e73f7728dde5f8a5f812927787fe80e6a99a119
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2020-07-08 22:18:04 +0900

    event syncing

--- posts/index.js
+++ posts/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
-const cors = require('cors');
 const axios = require('axios');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
